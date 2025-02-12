2fba8b23046f14ca41b82b0ed9d7d64ac5264522
h.elinany@gomobile.ma <h.elinany@gomobile.ma>
2025-02-12 12:40:32 +0100

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white` } 
       >
-            <Navbar></Navbar>
+            {/* <Navbar></Navbar> */}
             {/* <Dash></Dash> */}
             {/* <TaskBoard></TaskBoard>            */}
         {children}
