bc086ab0bb1208678a66b4ec9010cb4491de4aab
h.elinany@gomobile.ma <h.elinany@gomobile.ma>
2025-02-12 12:49:51 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
-import Image from "next/image";
-// import TaskBoard from "./components/Taskboard/taskBoard";
-// import Login from "./components/loging/loging";
-// import TodoApp from "./components/register/register";
-// import Register from "./components/register/register";
-// import dashPage from "./components/dashboard/home"
-// import HomeD from "./components/dashboard/home";
-// import Navbar from "./components/dashboard/navbar";
-import { Layout } from "lucide-react";
+// import Image from "next/image";
+// // import TaskBoard from "./components/Taskboard/taskBoard";
+// // import Login from "./components/loging/loging";
+// // import TodoApp from "./components/register/register";
+// // import Register from "./components/register/register";
+// // import dashPage from "./components/dashboard/home"
+// // import HomeD from "./components/dashboard/home";
+// // import Navbar from "./components/dashboard/navbar";
+// import { Layout } from "lucide-react";
 
-export default function Home() {
-  return (
-    <div></div>
-    // <Layout></Layout>
+// export default function Home() {
+//   return (
+//     <div></div>
+//     // <Layout></Layout>
   
     
 
-    // <TaskBoard></TaskBoard>
+//     // <TaskBoard></TaskBoard>
 
-    // <TodoApp></TodoApp>
-    // <Register></Register>
+//     // <TodoApp></TodoApp>
+//     // <Register></Register>
 
-    // <HomeD></HomeD>
+//     // <HomeD></HomeD>
 
-  )
-}
+//   )
+// }
