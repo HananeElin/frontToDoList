5d613c9c1382c84a97e6ab8c1789afd7eaa5550f
h.elinany@gomobile.ma <h.elinany@gomobile.ma>
2025-02-24 15:06:31 +0000

    fix: fix header

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,22 @@
 "use client";
-import React from "react";
+import React, { use, useEffect } from "react";
 import { FaSignInAlt, FaUserPlus, FaCheck, FaSignOutAlt } from "react-icons/fa";
 import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Navbar() {
   const router = useRouter();
+
+  const { user, logout } = useAuth();
+
+
+
+  useEffect(() => {
+    if (user) {
+      console.log("User is logged in:", user);
+    }
+  }
+    , [user]);
 
   return (
     <nav className="bg-black p-4 flex justify-between items-center shadow-md">
@@ -13,24 +25,24 @@ export default function Navbar() {
         To Do <FaCheck />
       </button>
       <div className="flex space-x-2">
-        <button 
+        {user ? <><button
           className="bg-white text-black px-4 py-2 rounded-lg shadow-md flex items-center gap-2 hover:bg-gray-200 transition duration-300"
           onClick={() => router.push("/register")}
         >
           <FaUserPlus /> Sign Up
         </button>
-        <button 
-          className="bg-red-500 text-white px-4 py-2 rounded-lg shadow-md flex items-center gap-2 hover:bg-red-600 transition duration-300"
-          onClick={() => router.push("/login")}
-        >
-          <FaSignInAlt /> Log In
-        </button>
-        <button 
-          className="bg-white text-black px-4 py-2 rounded-lg shadow-md flex items-center gap-2 hover:bg-gray-200 transition duration-300"
-          onClick={() => router.push("/home")}
-        >
-         <FaSignOutAlt /> Signout
-        </button>
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-lg shadow-md flex items-center gap-2 hover:bg-red-600 transition duration-300"
+            onClick={() => router.push("/login")}
+          >
+            <FaSignInAlt /> Log In
+          </button></> : <button
+            className="bg-white text-black px-4 py-2 rounded-lg shadow-md flex items-center gap-2 hover:bg-gray-200 transition duration-300"
+            onClick={() => router.push("/login")}
+          >
+          <FaSignOutAlt /> Signout
+        </button>}
+
       </div>
     </nav>
   );
